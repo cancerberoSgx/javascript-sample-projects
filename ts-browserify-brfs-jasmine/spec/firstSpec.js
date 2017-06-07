bf9f3b2134283b10df281e552f4e17057887642c
cancerberosgx <sebastigurin@gmail.com>
2017-06-07 13:07:56 -0300

    @types

--- ts-browserify-brfs-jasmine/spec/firstSpec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Point_1 = require("../src/basic/Point");
-describe('1', function () {
-    it('2', function () {
-        var p = new Point_1.Point(1, 2);
-        expect(p.dist() < 3).toBe(true);
-    });
-});
