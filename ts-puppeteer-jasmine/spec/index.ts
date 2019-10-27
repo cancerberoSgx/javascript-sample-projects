11a4196cdcf825b17396915d69587b9c1d643b76
Sebastián Gurín <sebastigurin@gmail.com>
2019-10-27 11:49:40 -0300

    ts-jasmine-p

--- ts-puppeteer-jasmine/spec/index.ts
+++ ts-puppeteer-jasmine/spec/index.ts
@@ -1,6 +1,6 @@
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
 
-require('../../node_modules/jasmine-expect/src/index.js') // hack to load jasmine-matchers before out specs so it gets bundled
+require('../../node_modules/jasmine-expect') // hack to load jasmine-matchers before out specs so it gets bundled
 
-export * from './testAssetsSpec'
+export * from './assetsSpec'
 export * from './indexSpec'
