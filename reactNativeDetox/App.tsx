249e9f3b87d5ae9073dbb4e2ae6507b777fc611f
Sebastian Gurin <sebastian.gurin@simplex.software>
2021-07-14 15:52:26 -0300

    react-native-detox

---
+++ reactNativeDetox/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+import {
+  Colors,
+  Header
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [screen, setScreen] = useState('first');
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          {screen==='first' && <View>
+            <Text testID="screen-title">Screen 1</Text>
+            <Button testID="screen-button" title="click me 1" onPress={() =>setScreen('second')}/>
+          </View>}
+          {screen==='second' && <View>
+            <Text testID="screen-title">Screen 2</Text>
+            <Button testID="screen-button" title="click me 2" onPress={() =>setScreen('first')}/>
+          </View>}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
